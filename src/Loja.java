fa2e7af0535ae598112b03511468a70bcd2dbcea
Thiago7748 <tgr.20001@gmail.com>
2025-05-07 10:23:18 -0300

    build: Primeias implementações com Arrays

--- src/Loja.java
+++ src/Loja.java
@@ -5,21 +5,24 @@ public class Loja {
     private double salarioBaseFuncionario;
     private Endereco endereco;
     private Data dataFundacao;
+    private Produto[] estoqueProdutos;
 
-    public Loja(String nome, int quantidadeFuncionarios, double salarioBaseFuncionario, Endereco endereco, Data dataFundacao){
+    public Loja(String nome, int quantidadeFuncionarios, double salarioBaseFuncionario, Endereco endereco, Data dataFundacao, int quantidadeMaximaProdutos){
         this.nome = nome;
         this.quantidadeFuncionarios = quantidadeFuncionarios;
         this.salarioBaseFuncionario = salarioBaseFuncionario;
         this.endereco = endereco;
         this.dataFundacao = dataFundacao;
+        this.estoqueProdutos = new Produto[quantidadeMaximaProdutos]; // Inicializa o array com o tamanho especificado
     }
 
-    public Loja(String nome, int quantidadeFuncionarios, Endereco endereco, Data dataFundacao){
+    public Loja(String nome, int quantidadeFuncionarios, Endereco endereco, Data dataFundacao, int quantidadeMaximaProdutos){
         this.nome = nome;
         this.quantidadeFuncionarios = quantidadeFuncionarios;
         this.salarioBaseFuncionario = -1.0;
         this.endereco = endereco;
         this.dataFundacao = dataFundacao;
+        this.estoqueProdutos = new Produto[quantidadeMaximaProdutos]; // Inicializa o array com o tamanho especificado
     }
 
     public void setNome(String nome){
@@ -57,6 +60,14 @@ public class Loja {
         this.dataFundacao = dataDeFundacao;
     }
 
+    // Método de acesso para o estoqueProdutos
+    public Produto[] getEstoqueProdutos() {
+        return this.estoqueProdutos;
+    }
+    public void setEstoqueProdutos(Produto[] estoqueProdutos) {
+        this.estoqueProdutos = estoqueProdutos;
+    }
+
     public double gastosComSalario(){
         if (this.salarioBaseFuncionario == -1){
             return -1.0;
@@ -75,13 +86,46 @@ public class Loja {
         }
     }
 
+    // Método para imprimir todos os produtos no estoque
+    public void imprimeProdutos() {
+        System.out.println("Produtos em estoque na loja " + this.nome + ":");
+        for (int i = 0; i < estoqueProdutos.length; i++) {
+            if (estoqueProdutos[i] != null) {
+                System.out.println(estoqueProdutos[i].toString());
+            }
+        }
+    }
+
+    // Método para inserir um produto no estoque
+    public boolean insereProduto(Produto produto) {
+        for (int i = 0; i < estoqueProdutos.length; i++) {
+            if (estoqueProdutos[i] == null) {
+                estoqueProdutos[i] = produto;
+                return true; // Produto inserido com sucesso
+            }
+        }
+        return false; // Não há espaço disponível no estoque
+    }
+
+    // Método para remover um produto do estoque pelo nome
+    public boolean removeProduto(String nomeProduto) {
+        for (int i = 0; i < estoqueProdutos.length; i++) {
+            if (estoqueProdutos[i] != null && estoqueProdutos[i].getNome().equals(nomeProduto)) {
+                estoqueProdutos[i] = null;
+                return true; // Produto removido com sucesso
+            }
+        }
+        return false; // Produto não encontrado no estoque
+    }
+
     @Override
     public String toString() {
         String info = "Loja: " + nome + "\n" +
                 "Quantidade de Funcionários: " + quantidadeFuncionarios + "\n" +
                 "Salário Base Funcionário: R$" + String.format("%.2f", salarioBaseFuncionario) + "\n" +
                 "Endereço:\n" + endereco + "\n" +
-                "Data de Fundação: " + dataFundacao;
+                "Data de Fundação: " + dataFundacao + "\n" +
+                "Capacidade do Estoque: " + estoqueProdutos.length + " produtos";
         return info;
     }
 }
